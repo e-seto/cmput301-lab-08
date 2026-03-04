39372b91cf329d9eb456fa12c18fcfdf8e05fa4c
Elizabeth Seto <135999989+e-seto@users.noreply.github.com>
2026-03-03 17:54:44 -0700

    add: deleteCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -16,11 +16,13 @@ public class CustomList {
         cities.add(city);
     }
 
+    // Will be implemented later using TDD (leave blank for now)
+    // public boolean hasCity(City city) { ... }
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
-    
 
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
 }
