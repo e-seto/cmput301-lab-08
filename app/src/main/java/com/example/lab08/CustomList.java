48af236eb07c8e4c2e8e60338cbc0c02dc2e541f
Elizabeth Seto <135999989+e-seto@users.noreply.github.com>
2026-03-03 17:42:20 -0700

    add: hasCity method to CustomList

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -7,13 +7,18 @@ public class CustomList {
     private List<City> cities;
 
     public CustomList() {
+
         this.cities = new ArrayList<>();
     }
 
     public void addCity(City city) {
+
         cities.add(city);
     }
 
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
     
 
     // Will be implemented later using TDD (leave blank for now)
